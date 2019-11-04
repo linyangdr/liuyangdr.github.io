26f48a7d13b1b1f990b2c823bef48e61655399fc
linyangdr <56680029+linyangdr@users.noreply.github.com>
2019-11-04 22:01:13 +0800

    Update set.js

--- js/set.js
+++ js/set.js
@@ -1,20 +1,58 @@
-
-let mycode = (code) => {
-    let domcode = document.querySelector("#code")
+let mycode = (prefix, code, fn) => {
+    let domCode = document.querySelector('#code')
     let n = 0
-    var clock = setInterval( () => {
+    var clock = setInterval(() => {
         n += 1
-        domcode.innerHTML = code.substring(0,n)
-        myStyle.innerHTML = code.substring(0,n)
-
-        if ( n > code.length) {
+        domCode.innerHTML = Prism.highlight(prefix + code.substring(0, n), Prism.languages.css)
+        myStyle.innerHTML = prefix + code.substring(0, n)
+        domCode.scrollIntoView({behavior: "instant", block: "end", inline: "nearest"})
+        if (n >= code.length) {
             window.clearInterval(clock)
+            fn.call()
         }
-    },60)
+    }, 50)
+}
+
+let createPaper = fn => {
+    var paper = document.createElement('pre');
+    paper.id = 'paper';
+    document.querySelector('#box').appendChild(paper);
+    fn.call();
+};
+
+let writeMarkdown = (markdown, fn) => {
+    let domPaper = document.querySelector('#paper')
+    let n = 0
+    let clock = setInterval(() => {
+        n += 1
+        domPaper.innerHTML = Prism.highlight(markdown.substring(0, n), Prism.languages.markdown)
+        domPaper.innerHTML = marked(markdown.substring(0, n))
+        domPaper.scrollIntoView({behavior: "instant", block: "end", inline: "nearest"})
+        if (n >= markdown.length) {
+            window.clearInterval(clock)
+            fn.call()
+        }
+    }, 45)
+}
+
+let convertMarkdownToHtml = fn => {
+    // document.querySelector('#paper').innerHTML = marked(markdown)
+    fn.call()
 }
 
 let main = () => {
-    mycode(cssCode)
-}
+    mycode('', cssCode, () => {
+        createPaper(() => {
+            mycode(cssCode, htmlCode, () => {
+                writeMarkdown(markdown, () => {
+                    convertMarkdownToHtml(() => {
+                    })
+                })
+            })
+        })
+    })
+};
+
+
 
 main()
