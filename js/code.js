566fedaa9eeb086add2a4c1d977fa88baabffada
linyangdr <56680029+linyangdr@users.noreply.github.com>
2019-11-04 21:57:02 +0800

    Add files via upload

--- js/code.js
+++ js/code.js
@@ -1,59 +1,84 @@
-! (function () {
-    let cssCode = window.cssCode = `
-/* Hello？*/
-/* 没想到我会做这样的网页吧啊哈哈哈*/
-/* 这个网页就当做是给你的生日礼物了之前几年什么都没有给*/
-/* 下面要说些什么呢？改一下格式先*/
+!function () {
+	let cssCode = window.cssCode = `
+/*22班的同学们好啊！
+好久不见甚是想念。*/
+/*浏览之前，先改一下格式orz*/
 *{
 	transition: all .5s;
-}  
-/*换一下背景颜色，当然要换成我最喜欢的紫色哈哈哈*/
+}
+/*先把背景颜色改成我最喜欢的紫色哈哈哈*/
 html{
 	background: #F6EAFC;
 }
-/*不如让我们给它加一个边框吧*/
+/*不妨让我们给它加一个边框吧*/
 #code{
-    border: 3px solid #AC25F0;
-    padding: 30px;
+    border: 2px solid #AC25F0;
+    padding: 20px;
 }
-/*全都是我最喜欢的紫色哈哈哈*/
-/*接下来要干什么呢？
-当然是要祝你生日快乐了啊！祝宇宙无敌长腿美少女王然然生日快乐啊！！*/
-<br>
-<img src="sheng.jpg" style=" width: 300px; height: 200px">
-<br>
-/* 生日快乐快乐快乐快乐要19岁咯，步入大学二个月或多或少有许多感受
-<br>
-希望你能一直保持刚入大学时的新鲜感，努力成为一个更好的人啊（对齐）
-我在这边就遇到了咱们河南的一个学长人挺好的给我们讲知识（对齐对齐）
-包括这个网页就是他教我怎么做的哈哈哈(强行对齐强行对齐强行对齐对齐）
-他的博客里写了这么几句话我觉得挺有感悟也想让你瞅瞅：*/
-<br>
-/*1.大学校园是座象牙塔 呆久了 都不知道自己几斤几两了
-<br>
-2.不要悲观 失败又能怎样 至少积累了不少经验 做就是了
-<br>
-3.并非明星才会受到瞩目 而是受到瞩目的人成为明星    */
-/*今天是你的生日本不应该说这样的话但是这几句话真的是
-<br>
-挺好的很让我有感触希望你也能理解这几句话哈哈*/
-<br>
-/*真心地再次由衷地祝福你生日快乐祝你也祝我一起努力高考不理想那就在大学大放光彩
-也希望你会变成你想成为的那个人同时也不要忘记多陪伴父母啊大学四年一路顺风
-我们河南出来的都不会差祝你快乐啊每天开心！*/
-<br>
-/* 最后带你看一下我在这边拍的照片吧*/
-<br>
-<img src="tu.jpeg" style="width: 300px;height: 200px">
-/* 我们学校的图书馆虽然不是正门据传言它是亚洲最长（我进去后迷路了）*/
+/*改一下字体颜色？*/
+#code{
+    color: #0930FC;
+}
+/*来点动画吧*/
+#code{
+    animation: Myfirst 4s linear infinite;
+}
+/*现在让你们了解一下我们的学校吧*/
+/*Welcome to TUPJ.*/
+/*那么先让我转换一下页面吧*/
+`;
+	let htmlCode = window.htmlCode =`
+#code{
+     display:inline-block;
+     position:fixed;
+     left:0;
+     width: 40%;
+     height: 80%;
+     margin-left: 35px;
+    }	
+#paper{
+    position: relative;
+    display: inline-block;
+    width: 45%;
+    height: 80%;
+    background: linear-gradient(to bottom, #A7FEF1, #20EEF3); 
+    padding: 20px;
+    box-shadow: 0 2px 10px 1px rgba(0, 0, 0, 0.8);
+    text-shadow: 0 1px 0 #F6EF97;
+    animation:myfirst1 3s;
+    overflow: auto;
+}  
+/*哎哟喂你看它在乱动！*/
+/*那就让它安稳点。*/
+/*是时候让它归位了orz*/
+#paper{
+    position:fixed;
+    right:0;
+    margin-right:40px;
+}  
+/*Perfect！*/
+  `;
+
+	let markdown = window.markdown = `
+# 天津工业大学简称天工大。
+## 校训：严谨、严格、求实、求是.
+## 学校官网链接 [link](http://www.tjpu.edu.cn/).
+## 值得一提的是我们学校的纺织专业位列全国第一.
+
+# 朝迎海河晨曦  暮浴津门霞光.
+<img src="tu1.jpg" style="width: 500px;height: 200px">
+我们学校的图书馆虽然不是正门据传言它是亚洲最长（我进去后迷路了）
 <br>
 <img src="ye.jpg" style="width: 300px;height: 200px">
-/*这是我们学校晚上的夜景（吐槽一下晚上黑灯瞎火啥也看不见）/*
+这是我们学校晚上的夜景（吐槽一下晚上黑灯瞎火啥也看不见）
 <br>
 <img src="pm.jpeg" style="width: 300px;height: 200px">
-/*望星运动场是我们寝室旁边的虽说我没怎么去过大活动都在这里*/
+望星运动场是我们寝室旁边的虽说我没怎么去过大活动都在这里
 <br>
-/*那就先到这里啦，这个网页我是会定期更新的咯会把我所学的都在这里展示
-随时都可以来看啊，你的生日那么这个网址就专属于你了，但是内容只挂一天哦*/
-(有可能我懒得改哈哈哈）`;
-})()
+而我在计算机与软件技术学院，实属荣幸。
+<br>
+春览静园绿景，秋听泮湖吟唱。
+`;
+
+
+}.call()
